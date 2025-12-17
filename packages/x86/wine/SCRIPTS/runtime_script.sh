75f0eff67301c79c3bcc665c3c599775b30de156
b-e-n-z1342 <quasarfoks@proton.com>
2025-12-17 23:40:51 +0300

    Макет репозитория для FreeBSD-apploader

---
+++ packages/x86/wine/SCRIPTS/runtime_script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+parsing_packege_list() {
+    packegelist="$(cat ../DEPENDENCIES)"
+
+}
+
+patch_or_variables() {
+
+
+}
+
+packege_install() {
+    apploader install "${packegelist}"
+
+}
+main() {
+
+}
+main
